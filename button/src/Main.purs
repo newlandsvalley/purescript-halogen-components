0c4639e0ab677e9f1c7d9a1a29c68ddd3fa8e4ff
John Watson <john.watson@gmx.co.uk>
2018-04-07 12:24:49 +0100

    amend button example
    to exercise both the static and toggled label variants.

--- button/src/Main.purs
+++ button/src/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Aff.Console (CONSOLE, log)
-import Halogen.SimpleButtonComponent (component, Message(..)) as B
+import Halogen.SimpleButtonComponent (component, toggledLabelComponent, Message(..)) as B
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Control.Coroutine as CR
@@ -14,7 +14,8 @@ main :: Eff (HA.HalogenEffects (console :: CONSOLE)) Unit
 main = HA.runHalogenAff
   do
     body <- HA.awaitBody
-    io <- runUI (B.component "press me" ) unit body
+    -- io <- runUI (B.component "press me" ) unit body
+    io <- runUI (B.toggledLabelComponent "start" "stop" ) unit body
 
     io.subscribe $ CR.consumer \(B.Toggled bool) -> do
       log $ "button pressed on: " <> (show bool)
